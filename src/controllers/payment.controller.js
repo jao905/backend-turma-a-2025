64ed1a4be2d0bbc907b8d49f713f652889af1f01
JOAO PEDRO OLIVEIRA JORDAO <161616023+jao905@users.noreply.github.com>
2025-02-26 14:10:38 +0000

    [feat] Controller zod

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,9 +1,31 @@
+import {z} from "zod";
+const paymentSchema = z.object({
+    data: z.string().datetime(), 
+    numerorecibo: z.string().int().positive(),
+    valor: z.number().positive(),
+    observacao: z.string().max(100),
+}); 
+
 const paymentController = {
     async createPayment(req, res) {
         try {
+            const{nome, email, senha} = req.body;
+            paymentSchema.parse({nome, email, senha});
+            console.log({nome, email, senha});
+            // Aqui você pode fazer a lógica para salvar no banco de dados
             res.status(201).json({message: "Payment create sucessfuly"});
         } catch (error) {
-            console.log(error);
+            if (error instanceof z.ZodError) {
+                return res.status(400).json({
+                    message: "Validation error",
+                    errors: error.errors.map(
+                        err => ({ 
+                            atributo: err.path[0],
+                            mensagem: err.message
+                        })
+                    )
+                });
+            }
             res.status(500).json({message: error.mesage});
             
         }
