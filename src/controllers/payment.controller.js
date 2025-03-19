34dcc6fee9d893864422e960781e4e96d0d0d63a
JOAO PEDRO OLIVEIRA JORDAO <161616023+jao905@users.noreply.github.com>
2025-03-19 14:00:06 +0000

    [feat] Update payment controller

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,9 +1,9 @@
 import {z} from "zod";
 const paymentSchema = z.object({
     data: z.string().datetime(), 
-    numerorecibo: z.string().int().positive(),
     valor: z.number().positive(),
-    observacao: z.string().max(100),
+    numero: z.number().int().positive(),
+    observacao: z.string().optional(),
 }); 
 
 const paymentController = {
@@ -29,7 +29,33 @@ const paymentController = {
             res.status(500).json({message: error.mesage});
             
         }
+    },
+   async updatePayment(req, res) {
+   try {
+    const {id} = req.params;
+    const {valor, numero, data, observacao} = req.body;
+    paymentSchema.parse({valor, numero, data, observacao});
+    console.log({valor, numero, data, observacao});  
+    res.status(200).json({message: 'Payment updated sucessfuly',
+                         data: {id, valor, numero, data, observacao }});                   
+   } catch (error) {
+    if (error instanceof z.ZodError) {
+       return res.status(400).json({message: 'Validation error',
+          details:error.errors});
     }
-}
+    return res.status(500).json({message: error.message});
+   }
+   },
+
+   async deletePayment(req, res) {
+    try {
+        const {id} = req.params;
+       return res.status(200).json({message: 'Payment deleted sucessfuly', id});
+    } catch (error) {
+       return res.status(500).json({message: "Internal server error"});
+    }
+   },
+};
+
 
 export default paymentController;
